c8b118e3c1a1183c9608cb25963a926478e29f81
Anderson Medeiros <anderson.santos.medeiros@hotmail.com>
2023-06-27 20:23:33 -0300

    feat: initial template

---
+++ template/README.md
@@ -0,0 +1,19 @@
+This is a test readme
+
+This lib was created by using the `npx create-js-lib` command
+
+TODO: Document each command and how to use them
+
+TODO: Add support to JSDoc
+
+https://prettier.io/docs/en/options.html
+
+https://eslint.org/docs/latest/use/configure/
+
+https://babeljs.io/docs/configuration
+
+https://babeljs.io/docs/configuration
+
+https://jsdoc.app/
+
+npm link
---
+++ template/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+/**
+ * @type {import('webpack').Configuration}
+ */
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'lib'),
+    filename: 'main.js',
+    clean: true,
+    globalObject: 'this',
+    library: {
+      name: 'my-webpack-library',
+      type: 'umd',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: 'asset',
+      },
+    ],
+  },
+  externals: {
+    // Put here any dependency that you want to set as peerDependency
+    // https://webpack.js.org/configuration/externals/#root
+  },
+};
