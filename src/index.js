e3eafd0bcb9c520b49d2e1da8c4b431aded22380
Anderson Medeiros <anderson.santos.medeiros@hotmail.com>
2023-10-01 18:48:20 -0300

    chore: adds eslint, prettier and husky to project

--- src/index.js
+++ src/index.js
@@ -1,87 +1,88 @@
 #!/usr/bin/env node
 
-const colors = require("@colors/colors/safe");
-const prompt = require("prompt");
-const nunjucks = require("nunjucks");
-const fs = require("fs");
-const fse = require("fs-extra");
-const path = require("path");
-const spawn = require("cross-spawn");
-const commandExistsSync = require("command-exists").sync;
+const colors = require('@colors/colors/safe');
+const prompt = require('prompt');
+const nunjucks = require('nunjucks');
+const fs = require('fs');
+const fse = require('fs-extra');
+const path = require('path');
+const spawn = require('cross-spawn');
 
-const templateDir = path.join(__dirname, "..", "template");
+const commandExistsSync = require('command-exists').sync;
+
+const templateDir = path.join(__dirname, '..', 'template');
 
 const dottedFilesAndFolders = [
-  "babelrc",
-  "eslintrc.json",
-  "gitignore",
-  "prettierrc",
-  "husky",
-  "vscode",
+  'babelrc',
+  'eslintrc.json',
+  'gitignore',
+  'prettierrc',
+  'husky',
+  'vscode',
 ];
 
 const schema = {
   properties: {
     name: {
-      description: colors.white("lib name"),
-      type: "string",
+      description: colors.white('lib name'),
+      type: 'string',
       pattern: /^\S+$/,
-      message: "provide a valid name without any empty spaces",
+      message: 'provide a valid name without any empty spaces',
       required: true,
     },
     version: {
-      description: colors.white("initial version"),
-      type: "string",
+      description: colors.white('initial version'),
+      type: 'string',
       pattern: /^\d+\.\d+\.\d+$/,
-      message: "provide a valid version",
-      default: "0.0.1",
+      message: 'provide a valid version',
+      default: '0.0.1',
     },
     description: {
-      description: colors.white("description"),
-      type: "string",
+      description: colors.white('description'),
+      type: 'string',
     },
     repositoryUrl: {
-      description: colors.white("git repository url"),
-      type: "string",
+      description: colors.white('git repository url'),
+      type: 'string',
       pattern: /^https?:\/\/[^\s/$.?#].[^\s]*$/,
-      message: "provide a valid repository url",
+      message: 'provide a valid repository url',
     },
     keywords: {
-      description: colors.white("keywords"),
-      type: "string",
+      description: colors.white('keywords'),
+      type: 'string',
     },
     author: {
-      description: colors.white("author"),
-      type: "string",
+      description: colors.white('author'),
+      type: 'string',
     },
     packageManager: {
-      description: colors.white("install dependencies with npm or yarn"),
-      type: "string",
+      description: colors.white('install dependencies with npm or yarn'),
+      type: 'string',
       required: true,
-      default: "npm",
+      default: 'npm',
     },
   },
 };
 
-prompt.message = "";
+prompt.message = '';
 
-prompt.delimiter = colors.white(":");
+prompt.delimiter = colors.white(':');
 
 prompt.start();
 
 console.log(
-  colors.white("This utility will walk you through creating a new lib")
+  colors.white('This utility will walk you through creating a new lib')
 );
 console.log(
   colors.white(
-    "It will ask you some questions and use them to build the boilerplate\n"
+    'It will ask you some questions and use them to build the boilerplate\n'
   )
 );
 
 prompt.get(schema, (err, result) => {
   if (err) throw new Error(err);
 
-  if (!commandExistsSync("git"))
+  if (!commandExistsSync('git'))
     throw new Error(
       `The command git doesn't exist on PATH, install it end try again`
     );
@@ -91,7 +92,7 @@ prompt.get(schema, (err, result) => {
       `The command ${result.packageManager} doesn't exist on PATH, install it end try again`
     );
 
-  console.log(colors.white("\nCreating boilerplate..."));
+  console.log(colors.white('\nCreating boilerplate...'));
 
   const projectDir = path.join(process.cwd(), result.name);
 
@@ -106,22 +107,22 @@ prompt.get(schema, (err, result) => {
 
   addDotToFilesAndFolders(result.name);
 
-  console.log(colors.white("\nInstalling dependencies...\n"));
+  console.log(colors.white('\nInstalling dependencies...\n'));
 
-  spawn.sync("git", ["init"], {
-    stdio: "ignore",
+  spawn.sync('git', ['init'], {
+    stdio: 'ignore',
     cwd: projectDir,
   });
 
-  spawn.sync(result.packageManager, ["install"], {
-    stdio: "inherit",
+  spawn.sync(result.packageManager, ['install'], {
+    stdio: 'inherit',
     cwd: projectDir,
   });
 
-  spawn.sync("npx", ["husky", "install"], {
-    stdio: "inherit",
+  spawn.sync('npx', ['husky', 'install'], {
+    stdio: 'inherit',
     cwd: projectDir,
-  })
+  });
 
   console.log(colors.white(`\nLib created successfully on ${projectDir}.\n`));
 });
@@ -155,13 +156,13 @@ function transformResultForRender(result) {
   return {
     ...result,
     keywords: result.keywords
-      ? result.keywords.split(",").map((keyword) => `"${keyword.trim()}"`)
+      ? result.keywords.split(',').map((keyword) => `"${keyword.trim()}"`)
       : undefined,
     pascalCasedName: result.name
-      .split("-")
-      .map((w) => w.split("_"))
+      .split('-')
+      .map((w) => w.split('_'))
       .flat()
       .map((w) => (w ? `${w[0].toUpperCase()}${w.slice(1)}` : w))
-      .join(""),
+      .join(''),
   };
 }
